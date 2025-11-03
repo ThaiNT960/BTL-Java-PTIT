aaae38affbd6a7f1c67b35af1143ec1e66e7fb97
thaint <truongthai960@gmail.com>
2025-11-03 23:10:09 +0700

    Update ProductDAO.java

--- demo/src/main/java/com/ptit/shop/dao/ProductDAO.java
+++ demo/src/main/java/com/ptit/shop/dao/ProductDAO.java
@@ -12,7 +12,7 @@ public class ProductDAO {
 
     private static final String URL  =
             "jdbc:mysql://localhost:3306/ptitshop?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC";
-    private static final String USER = "test";
+    private static final String USER = "admin";
     private static final String PASS = "124";
 
     private Connection getConnection() throws SQLException {
